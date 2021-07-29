146b061d2b8929eccf1bc0ae812fef4c89c2ee69
Flavio Toshiuk Junior <flaviotoshiukhjr@outlook.com>
2021-07-29 01:18:12 +0100

    add lib react-spring

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
 import "./App.css";
+import { animated, useSpring } from "react-spring";
 
 const App = () => {
   const [word, setWord] = useState("");
 
   const isPalindrome =
     word === word.toLowerCase().replace(" ").split("").reverse().join("");
+
+  const { x } = useSpring({
+    from: { x: 0 },
+    x: isPalindrome ? 1 : 0,
+    config: { duration: 1000 },
+  });
+
   return (
     <div className={`App ${isPalindrome ? "isPalindrome" : "isNotPalindrome"}`}>
       <div>
@@ -17,6 +25,21 @@ const App = () => {
           value={word}
           onChange={(e) => setWord(e.target.value)}
         ></input>
+        <animated.div
+          style={{
+            marginTop: "20px",
+            fontSize: "2em",
+            opacity: x.interpolate({ range: [0, 1], output: [0.3, 1] }),
+            transform: x
+              .interpolate({
+                range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+                output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1],
+              })
+              .interpolate((x) => `scale(${x})`),
+          }}
+        >
+          {isPalindrome ? "Yes it is!" : "No it's not :c"}
+        </animated.div>
       </div>
       <div className="credits">
         <p>
